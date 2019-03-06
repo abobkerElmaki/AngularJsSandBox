f3fa9e212124ecfef124ceb0f8629185a1e91321
abobker Elmaki <bkry@gmial.com>
2019-03-06 16:03:00 +0300

    adding bootstrap and NavBar for MainPage

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,3 @@
 
 angular
-  .module("angularApp", ['ngRoute','mainController'])
-  .config(function($routeProvider) {
-    $routeProvider.when("/", {
-      templateURL: "./index.html",
-      controller: "mainController"
-    });
-  });
+  .module("angularApp", [])
