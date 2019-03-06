49554dcd9a95d60c4c9497bc63ecdb365dbe06b3
abobker Elmaki <bkry@gmial.com>
2019-03-06 18:47:46 +0300

    adding two json data for foods and drinks and get it in UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,23 @@
 
-angular
-  .module("angularApp", [])
+var app =angular
+  .module("ResturantController", []);
+app.controller('ResturantController', function ($http, $scope) {
+
+    $scope.foods = [{}];
+    $scope.drinks = [{}];
+    $http.get('../Foods.json').then(function (response) {
+        var Foods = response.data;
+         $scope.foods = Foods;
+       //  console.log($scope.foods);
+    }
+        );
+
+    $http.get('../Drinks.json').then(function (response) {
+        var Foods = response.data;
+        $scope.drinks = Foods;
+     
+    }
+    );
+
+}
+  )
