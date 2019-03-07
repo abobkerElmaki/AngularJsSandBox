34d209f6e3110a56870997ad79c152cd6a241639
abobker Elmaki <bkry@gmial.com>
2019-03-07 13:11:12 +0300

    add Picks to specific item selected in foods

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,9 @@ app.controller('ResturantController', function ($http, $scope) {
     $scope.drinks = [{}];
     $http.get('../Foods.json').then(function (response) {
         var Foods = response.data;
-         $scope.foods = Foods;
+        $scope.foods = Foods;
+        $scope.pick = false;
+        $scope.itemOrder = [{}];
        //  console.log($scope.foods);
     }
         );
@@ -18,6 +20,29 @@ app.controller('ResturantController', function ($http, $scope) {
      
     }
     );
+    $scope.ShowPicks = function (item) {
+        $scope.pick = true;
+        $scope.itemOrder = item;
+    }
+
+    $scope.selection = [];
+    $scope.toggleSelection = function toggleSelection(picks) {
+        var idx = $scope.selection.indexOf(picks);
+        if (idx > -1) {
+    
+            $scope.selection.splice(idx, 1);
+        }
+        else {
+        
+            $scope.selection.push(picks);
+        }
+    };
+
+
+
+
+
+
 
 }
   )
